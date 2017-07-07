0776e1fdb52baf8c4182366d9b2b5795c7a47df5
sanekyy <sanekyy@gmail.com>
2017-07-07 12:31:34 +0300

    messenger done

--- src/main/java/ru/spbstu/telematics/messengerServer/Main.java
+++ src/main/java/ru/spbstu/telematics/messengerServer/Main.java
@@ -1,30 +1,15 @@
 package ru.spbstu.telematics.messengerServer;
 
 
-import org.apache.commons.io.FileUtils;
 import ru.spbstu.telematics.messengerServer.exceptions.ProtocolException;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.io.IOException;
-import java.util.zip.GZIPInputStream;
 
 /**
  * Created by ihb on 17.06.17.
  */
 public class Main {
     public static void main(String[] args) throws IOException, ProtocolException {
-        //new Server().run();
-
-
-        GZIPInputStream gis = new GZIPInputStream(new ByteArrayInputStream(FileUtils.readFileToByteArray(new File("/home/ihb/Documents/Projects/IdeaProjects/messengerServer/package"))));
-        byte[] buff = new byte[500];
-        gis.read(buff);
-
-
-        String str = new String(buff);
-
-        System.out.println(str);
-
+        new Server().run();
     }
 }
